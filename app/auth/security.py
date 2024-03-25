bee1dc537409c1e30b68ee8e8d923e4b07fce83e
Jonathan Balloch <jon.balloch@gmail.com>
2024-03-24 22:08:56 -0400

    working login infra

--- app/auth/security.py
+++ app/auth/security.py
@@ -1,11 +1,13 @@
 import os
 import datetime
 import uuid
+import json
 from urllib.parse import urlparse
 import webauthn
 from webauthn.helpers.structs import (
     AuthenticatorSelectionCriteria,
     UserVerificationRequirement,
+    PublicKeyCredentialDescriptor,
 )
 
 from flask import Blueprint, request, current_app
@@ -19,6 +21,9 @@ REDIS_PORT = os.getenv("REDIS_PORT")
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 REGISTRATION_CHALLENGES = Redis(
     host=REDIS_HOST, port=REDIS_PORT, db=0, password=REDIS_PASSWORD
+)
+AUTHENTICATION_CHALLENGES = Redis(
+    host=REDIS_HOST, port=REDIS_PORT, db=1, password=REDIS_PASSWORD
 )
 
 
@@ -82,4 +87,59 @@ def verify_and_save_credential(user, registration_credential):
     current_app.logger.info('credential success') #, flush=True)
 
     db.session.add(credential)
+    db.session.commit()
+
+
+def prepare_login_with_credential(user):
+    """
+    Prepare the authentication options for a user trying to log in.
+    """
+    allowed_credentials = [
+        PublicKeyCredentialDescriptor(id=credential.credential_id)
+        for credential in user.credentials
+    ]
+
+    authentication_options = webauthn.generate_authentication_options(
+        rp_id=_hostname(),
+        allow_credentials=allowed_credentials,
+    )
+
+    AUTHENTICATION_CHALLENGES.set(user.uid, authentication_options.challenge)
+    AUTHENTICATION_CHALLENGES.expire(user.uid, datetime.timedelta(minutes=10))
+
+    return json.loads(webauthn.options_to_json(authentication_options))
+
+
+def verify_authentication_credential(user, authentication_credential):
+    """
+    Verify a submitted credential against a credential in the database and the
+    challenge stored in redis.
+    """
+    expected_challenge = AUTHENTICATION_CHALLENGES.get(user.uid)
+    stored_credential = (
+        WebAuthnCredential.query.with_parent(user).filter_by(
+            credential_id=webauthn.base64url_to_bytes(authentication_credential.id)
+        ).first()
+    )
+    
+    # This will raise if the credential does not authenticate
+    # It seems that safari doesn't track credential sign count correctly, so we just
+    # have to leave it on zero so that it will authenticate
+    webauthn.verify_authentication_response(
+        credential=authentication_credential,
+        expected_challenge=expected_challenge,
+        expected_origin=f"https://{_hostname()}",
+        expected_rp_id=_hostname(),
+        credential_public_key=stored_credential.credential_public_key,
+        credential_current_sign_count=0
+    )
+    
+    # After a successful authentication, expire the challenge so it can't be used again.
+    AUTHENTICATION_CHALLENGES.expire(user.uid, datetime.timedelta(seconds=1))
+
+    
+    # Update the credential sign count after using, then save it back to the database.
+    # This is mainly for reference since we can't use it because of Safari's weirdness.
+    stored_credential.current_sign_count += 1
+    db.session.add(stored_credential)
     db.session.commit()
--- app/auth/view.py
+++ app/auth/view.py
@@ -2,11 +2,17 @@ import datetime
 import json
 import uuid
 
-from flask import Blueprint, render_template, request, make_response, session, abort, current_app
+from flask import (
+    Blueprint, render_template, request, make_response, session, 
+    abort, current_app, url_for, redirect)
+from sqlalchemy import or_, func
 from sqlalchemy.exc import IntegrityError
-from webauthn.helpers.exceptions import InvalidRegistrationResponse
-from webauthn.helpers.structs import RegistrationCredential, PublicKeyCredentialCreationOptions
-from webauthn.helpers import base64url_to_bytes, bytes_to_base64url, parse_registration_credential_json
+from webauthn.helpers.exceptions import InvalidRegistrationResponse, InvalidAuthenticationResponse
+from webauthn.helpers.structs import (
+    AuthenticationCredential, PublicKeyCredentialCreationOptions)
+from webauthn.helpers import (
+    base64url_to_bytes, bytes_to_base64url, parse_registration_credential_json,
+    parse_authentication_credential_json)
 
 from models import db, User
 from auth import security
@@ -18,11 +24,6 @@ auth = Blueprint("auth", __name__, template_folder="templates")
 @auth.route("/register")
 def register():
     return render_template("auth/register.html")
-
-
-@auth.route("/login")
-def login():
-    return "Login user"
 
 
 @auth.route("/create-user", methods=["POST"])
@@ -71,25 +72,111 @@ def add_credential():
 
     user = User.query.filter_by(uid=user_uid).first()
 
-    # try:
-    security.verify_and_save_credential(user, registration_credential)
+    try:
+        security.verify_and_save_credential(user, registration_credential)
 
-    session["registration_user_uid"] = None
-    res = make_response('{"verified": true}', 201)
+        session["registration_user_uid"] = None
+        res = make_response('{"verified": true}', 201)
+        res.set_cookie(
+            "user_uid",
+            user.uid,
+            httponly=True,
+            secure=True,
+            samesite="strict",
+            max_age=datetime.timedelta(days=30), #TODO: change to less
+        )
+        
+        current_app.logger.info('success?') #, flush=True)
+
+        return res
+    except InvalidRegistrationResponse as e:
+        current_app.logger.error('Invalid registration response: ' + str(e))
+        abort(make_response('{"verified": false}', 400))
+
+
+@auth.route("/login", methods=["GET"])
+def login():
+    """Prepare to login user with passwordless auth"""
+    user_uid = request.cookies.get("user_uid")
+    user = User.query.filter_by(uid=user_uid).first()
+
+    # If not remembered, we render login page w/o username
+    if not user:
+        return render_template("auth/login.html", username=None, auth_options=None)
+    
+    # If remembered we prepare the with username and options
+    auth_options = security.prepare_login_with_credential(user)
+    session['login_user_uid'] = user.uid
+
+    return render_template("auth/login.html", username=user.username, auth_options=auth_options)
+
+
+@auth.route("/prepare-login", methods=["POST"])
+def prepare_login():
+    """Prepare login options for a user based on their username or email"""
+    username_or_email = request.form.get("username_email", "").lower()
+    # The lower function just does case insensitivity for us.
+    user = User.query.filter(
+        or_(
+            func.lower(User.username) == username_or_email,
+            func.lower(User.email) == username_or_email,
+        )
+    ).first()
+
+    # If no user matches, send back the form with an error message
+    if not user:
+        return render_template(
+            "auth/_partials/username_form.html", error="No matching user found"
+        )
+
+    auth_options = security.prepare_login_with_credential(user)
+
+    res = make_response(
+        render_template(
+            "auth/_partials/select_login.html",
+            auth_options=auth_options,
+            username=user.username,
+        )
+    )
+
+    # Set the user uid on the session to get when we are authenticating later.
+    session["login_user_uid"] = user.uid
     res.set_cookie(
         "user_uid",
         user.uid,
         httponly=True,
         secure=True,
         samesite="strict",
-        max_age=datetime.timedelta(days=30), #TODO: change to less
+        max_age=datetime.timedelta(days=30),
     )
-    
-    current_app.logger.info('success?') #, flush=True)
-
     return res
-    # except InvalidRegistrationResponse as e:
-    #     current_app.logger.error('Invalid registration response: ' + str(e))
-    #     abort(make_response('{"verified": false}', 400))
 
 
+@auth.route("/login-switch-user")
+def login_switch_user():
+    """Remove a remembered user and show the username form again."""
+    session["login_user_uid"] = None
+    res = make_response(redirect(url_for('auth.login')))
+    res.delete_cookie('user_uid')
+    return res
+
+
+@auth.route("/verify-login-credential", methods=["POST"])
+def verify_login_credential():
+    """Remove a remembered user and show the username form again."""
+    user_uid = session.get("login_user_uid")
+    user = User.query.filter_by(uid=user_uid).first()
+    if not user:
+        abort(make_response('{"verified": false}', 400))
+    
+    authetication_data = request.get_data()
+    # current_app.logger.info(f'authetication_data(type: {type(authetication_data)}): ' + str(authetication_data))
+    authetication_json_data = json.loads(authetication_data)
+
+    authentication_credential = parse_authentication_credential_json(authetication_json_data)
+    try:
+        security.verify_authentication_credential(user, authentication_credential)
+        return make_response('{"verified": true}')
+    except InvalidAuthenticationResponse as e:
+        current_app.logger.error('Invalid authentication response: ' + str(e))
+        abort(make_response('{"verified": false}', 400))
